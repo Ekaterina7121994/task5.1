0174a5be333520f7517f87ed5cf4e6a54c151678
Ekaterina <shi.e.m@yandex.ru>
2023-08-29 13:35:44 +0300

    Parameterized

--- src/test/java/ru/netology/task5/services/WeekendScheduleTest.java
+++ src/test/java/ru/netology/task5/services/WeekendScheduleTest.java
@@ -1,25 +1,23 @@
 package ru.netology.task5.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class WeekendScheduleTest {
-    @Test
-    public void example1() {
+
+    @ParameterizedTest
+    //   @CsvSource({
+    //         "3,10000,3000,20000",
+    //          "2,100000,60000,150000"
+    //})
+    @CsvFileSource(files = "src/test/resources/data.csv")
+    public void example(int expected, int income, int expenses, int threshold) {
         WeekendSchedule service = new WeekendSchedule();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    public void example2() {
-        WeekendSchedule service = new WeekendSchedule();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        Assertions.assertEquals(expected, actual);
-    }
-
 }
 
 
